a01dbe6b38229bf0042d5e21f473584c700e0e1f
panlf <liangfeng_pan@163.com>
2023-12-10 21:51:20 +0800

    对象内存布局

---
+++ src/main/java/com/plf/interview/jol/JolTest.java
@@ -0,0 +1,16 @@
+package com.plf.interview.jol;
+
+
+import org.openjdk.jol.info.ClassLayout;
+
+public class JolTest {
+    public static void main(String[] args) {
+        Object o = new Object();
+
+        System.out.println(ClassLayout.parseInstance(o).toPrintable());
+
+        synchronized (o){
+            System.out.println(ClassLayout.parseInstance(o).toPrintable());
+        }
+    }
+}
